36c17d49b3735891feca55b9747b15056c53bb4c
Vasil Tarashev <145684223+waskoooo@users.noreply.github.com>
2024-01-22 18:43:27 +0200

    Add files via upload

---
+++ lab_softuni_party.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+codes = set()
+
+for _ in range(n):
+    code = input()
+    codes.add(code)
+
+code = input()
+while code != "END":
+    if code in codes:
+        codes.remove(code)
+    code = input()
+
+print(len(code))
+for code in sorted(codes):
+    print(code)
